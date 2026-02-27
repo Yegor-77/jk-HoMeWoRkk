d4ea5e53ec25a0099753cba92380f5333de9a276
Yegor-77 <egorhmeluk77@gmail.com>
2026-02-27 18:08:28 +0100

    1,1

--- js/index.js
+++ js/index.js
@@ -1,15 +1,37 @@
-const img = document.querySelector(".lazy-img");
+const lazyImages = document.querySelectorAll("img[data-src]");
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+
+        img.src = img.dataset.src;
+
+        img.onload = () => {
+          img.classList.add("loaded");
+        };
+
+        observer.unobserve(img);
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+lazyImages.forEach((img) => {
+  observer.observe(img);
+});
+
+document.getElementById("loadAll").addEventListener("click", () => {
+  lazyImages.forEach((img) => {
+    if (!img.src) {
       img.src = img.dataset.src;
-
       img.onload = () => {
         img.classList.add("loaded");
       };
-
-      observer.unobserve(img);
     }
   });
 });
